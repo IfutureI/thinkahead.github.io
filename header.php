dc9a6c8528ea3cc8e89c615f1784ecb4c1aee0bb
IFutureI <80450421+IfutureI@users.noreply.github.com>
2021-03-11 15:25:53 +0530

    Update header.php

--- header.php
+++ header.php
@@ -73,7 +73,7 @@
   <div class="collapse navbar-collapse" id="navbarNav">
     <ul class="navbar-nav ml-auto">
       <li class="nav-item">
-        <a class="nav-link" href="https://ifuturei.github.io/thinkahead.github.io/">Home</a>
+        <a class="nav-link" href="index.php">Home</a>
       </li>
       <li class="nav-item">
         <a class="nav-link" href="about-us.php">About Us</a>
